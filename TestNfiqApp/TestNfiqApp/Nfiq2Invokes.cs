782a6e19552501d22a5147735b45cd5bf5e623ff
Vincent Duong <vduong94@outlook.com>
2021-02-17 11:08:45 -0500

    Test application for testing the NFIQ2 API dll

---
+++ TestNfiqApp/TestNfiqApp/Nfiq2Invokes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TestNfiqApp
+{
+    public class Nfiq2Invokes
+    {
+        //Imports
+        [DllImport(@"..\..\..\..\..\Binaries\Nfiq2Api.dll", CharSet=CharSet.Ansi)]
+        private static extern void GetNfiq2Version(ref int major, ref int minor, ref int patch, ref IntPtr ovcPtr);
+        public static string Nfiq2Version()
+        {
+            int major = 0, minor = 0, patch = 0;
+            IntPtr ovcPtr = IntPtr.Zero;
+            string ocv = "";
+            GetNfiq2Version(ref major, ref minor, ref patch, ref ovcPtr);
+
+            if (ovcPtr != IntPtr.Zero)
+                ocv = Marshal.PtrToStringAnsi(ovcPtr);
+            return string.Format("Major: {0}, Minor: {1}, Patch: {2}, OCV: {3}", major, minor, patch, ocv);
+        }
+
+        [DllImport(@"..\..\..\..\..\Binaries\Nfiq2Api.dll")]
+        private static extern IntPtr InitNfiq2(); //Returns a hash value, but don't really need to know it
+        public static bool InitializeNfiq2()
+        {
+            var intPtr = InitNfiq2();
+            return intPtr != IntPtr.Zero;
+        }
+
+        [DllImport(@"..\..\..\..\..\Binaries\Nfiq2Api.dll\Nfiq2Api.dll")]
+        private static extern int ComputeNfiq2Score(int fpos, ref byte[] pixels, int size, int width, int height, int ppi);
+        public static int GetNfiq2Score(int fpos, ref byte[] pixels, int size, int width, int height, int ppi)
+        {
+            return ComputeNfiq2Score(fpos, ref pixels, size, width, height, ppi);
+        }
+    }
+}
---
+++ TestNfiqApp/TestNfiqApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace TestNfiqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var path = "Assets\\sample_image.wsq";
+
+            //Adds a random pause and press enter
+            //Console.WriteLine("Enter anything:");
+            //string userName = Console.ReadLine();
+
+            //var wsqImg = File.ReadAllBytes(path);
+            var fs = File.OpenRead(path);
+            var wsqImg = new byte[fs.Length];
+            fs.Read(wsqImg, 0, wsqImg.Length);
+
+            Console.WriteLine("Is initialized: " + Nfiq2Invokes.InitializeNfiq2());
+            Console.WriteLine(Nfiq2Invokes.Nfiq2Version());
+
+            var score = Nfiq2Invokes.GetNfiq2Score(0, ref wsqImg, wsqImg.Length, 545, 622, 500);
+            Console.WriteLine("The score for sample_image.wsq is " + score);
+        }
+    }
+}
